915657d750e32c82861cd4d210053f3f222b1636
dawkaka <yousiph77@gmail.com>
2023-07-11 21:23:40 +0000

    sss

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("Hell world and shit");
+    let f = first_word_slice(&s);
+    println!("{}", f);
+    s.clear();
+}
+
+// fn first_word(s: &str) -> usize {
+//     let bytes: &[u8] = s.as_bytes();
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
+//     s.len()
+// }
+
+fn first_word_slice(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    return &s[..];
 }
