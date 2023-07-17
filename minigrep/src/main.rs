f75c4a0f1cf4a1facdc5b0dde4b803df8f33a12d
dawkaka <yousiph77@gmail.com>
2023-07-17 08:04:46 +0000

    save arguments

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,10 @@
 use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    if args.len() < 3 {
+        panic!("Not enough arguments!");
+    }
+    let query = &args[1];
+    let file_path = &args[2];
+    println!("query: {}, file_path: {}", query, file_path);
 }
